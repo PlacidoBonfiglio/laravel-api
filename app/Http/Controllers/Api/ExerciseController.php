5c5e7c65c8fb9783f3b75c0e1f34e625f31c364d
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-11-14 15:44:29 +0100

    added Api\ExerciseController

---
+++ app/Http/Controllers/Api/ExerciseController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Exercise;
+use Illuminate\Http\Request;
+
+class ExerciseController extends Controller
+{
+    public function index() {
+        $exercises = Exercise::all();
+        return response()->json(
+            [
+                "success" => true,
+                "results" => $exercises
+            ]
+        );
+    }
+}
